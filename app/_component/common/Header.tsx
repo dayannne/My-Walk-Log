9bd6104deb0871204730d9c97cc6ff5cf1bc3ba9
Da-Youn <dayoun202@gmail.com>
2024-06-21 13:22:43 +0900

    Design : Header 스타일 수정 #3

--- app/_component/common/Header.tsx
+++ app/_component/common/Header.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import Navigation from './Navigation';
@@ -5,8 +7,11 @@ import Navigation from './Navigation';
 const Header = () => {
   return (
     <header className='flex flex-col border-r-[1.5px] border-solid border-gray-200'>
-      <div className='flex items-center justify-center w-full h-20 p-1 border-b border-solid border-gray-200'>
-        <Link href='/'>
+      <div>
+        <Link
+          className='flex items-center justify-center w-full h-20 p-1 border-b border-solid border-gray-200'
+          href='/'
+        >
           <Image
             src='/icons/icon-logo-mini(default).svg'
             alt=''
@@ -16,6 +21,14 @@ const Header = () => {
         </Link>
       </div>
       <Navigation />
+      <div>
+        <Link
+          className='flex items-center justify-center w-full h-16 p-1 border-t border-solid border-gray-200'
+          href='/login'
+        >
+          <Image src='/icons/icon-login.svg' alt='' width={32} height={32} />
+        </Link>
+      </div>
     </header>
   );
 };
