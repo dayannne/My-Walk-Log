c27cad675c3fe9cdd531c15a00b8455a36defc04
Da-Youn <dayoun202@gmail.com>
2024-06-23 20:53:08 +0900

    Feat : 로그인 상태에 따른 버튼 필터링 및 로그아웃 기능 구현  #4

--- app/_component/common/Header.tsx
+++ app/_component/common/Header.tsx
@@ -2,14 +2,25 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import Navigation from './Navigation';
+import { useUserStore } from '@/app/store/user';
 
 const Header = () => {
+  const router = useRouter();
+  const { user, setUser } = useUserStore();
+
+  const handleLogout = () => {
+    //TODO: 로그아웃 확인 팝업 추가
+    alert('로그아웃 되었습니다.');
+    setUser(null);
+    router.refresh();
+  };
   return (
     <header className='flex flex-col border-r-[1.5px] border-solid border-gray-200'>
       <div>
         <Link
-          className='flex items-center justify-center w-full h-20 p-1 border-b border-solid border-gray-200'
+          className='flex h-20 w-full items-center justify-center border-b border-solid border-gray-200 p-1'
           href='/'
         >
           <Image
@@ -21,13 +32,34 @@ const Header = () => {
         </Link>
       </div>
       <Navigation />
-      <div>
-        <Link
-          className='flex items-center justify-center w-full h-16 p-1 border-t border-solid border-gray-200'
-          href='/login'
-        >
-          <Image src='/icons/icon-login.svg' alt='' width={32} height={32} />
-        </Link>
+      <div className='flex h-auto w-full flex-col items-center justify-center gap-2 border-t border-solid border-gray-200 p-4'>
+        {user ? (
+          <>
+            <Link href='/profile'>
+              <Image
+                className='rounded-full'
+                src={
+                  user.profileImage ? user.profileImage : '/icons/icon-user.svg'
+                }
+                alt=''
+                width={32}
+                height={32}
+              />
+            </Link>
+            <button onClick={handleLogout}>
+              <Image
+                src='/icons/icon-logout.svg'
+                alt=''
+                width={32}
+                height={32}
+              />
+            </button>
+          </>
+        ) : (
+          <Link href='/login'>
+            <Image src='/icons/icon-login.svg' alt='' width={32} height={32} />
+          </Link>
+        )}
       </div>
     </header>
   );
