905dfff0bb77b26cd9a7dd853ae50a9116701f6e
Da-Youn <dayoun202@gmail.com>
2024-06-23 21:18:17 +0900

    Fix : npm run build 에러 수정

--- prisma/context.ts
+++ prisma/context.ts
@@ -1,11 +1,6 @@
+// /prisma/context.ts
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const prisma = new PrismaClient();
 
-const client = globalThis.prisma || new PrismaClient();
-// node 환경이 production이 아닐 때 prisma에 client를 할당
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
-
-export default client;
+export default prisma;
