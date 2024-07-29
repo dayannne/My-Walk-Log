85e94224665d8cd7dc7113e7e18eff621f80a5d6
Da-Youn <dayoun202@gmail.com>
2024-07-29 16:13:21 +0900

    Feat : 일기 작성 API 구현 #9

---
+++ app/api/diary/write/route.ts
@@ -0,0 +1,77 @@
+import prisma from '@/prisma/context';
+import { NextResponse } from 'next/server';
+import { Prisma } from '@prisma/client';
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+
+    const { authorId, placeId, diaryImages, content, tags, weather } = body;
+
+    if (!authorId) {
+      return new Response(
+        JSON.stringify({
+          message: '잘못된 요청 : 로그인 상태 / 장소 정보 확인',
+        }),
+        {
+          status: 400,
+          headers: { 'Content-Type': 'application/json' },
+        },
+      );
+    }
+
+    if (!content || !weather || !placeId) {
+      return new Response(
+        JSON.stringify({
+          message: '필수 필드를 모두 입력해 주세요.',
+        }),
+        {
+          status: 400,
+          headers: { 'Content-Type': 'application/json' },
+        },
+      );
+    }
+
+    const diary = await prisma.diary.create({
+      data: {
+        authorId,
+        placeId,
+        diaryImages: diaryImages || [],
+        content,
+        weather,
+        tags: tags || [],
+      },
+    });
+
+    return NextResponse.json(
+      { data: diary, message: '일기가 기록되었습니다.' },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error('일기 작성 오류:', error);
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        return new Response(
+          JSON.stringify({
+            message: '중복된 값이 존재합니다.',
+          }),
+          {
+            status: 409,
+            headers: { 'Content-Type': 'application/json' },
+          },
+        );
+      }
+    }
+
+    return new Response(
+      JSON.stringify({
+        message: '서버 내부 오류',
+      }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
+  }
+}
