fca8aa15e6c9b05f2506013ef757564bf19b594a
Da-Youn <dayoun202@gmail.com>
2024-08-19 15:48:52 +0900

    Feat : useModal, ConfirmModal, MenuModal 구현

--- styles/MaterialProvider.tsx
+++ styles/MaterialProvider.tsx
@@ -1,15 +1,23 @@
 'use client';
 
 import React from 'react';
-import { ThemeProvider } from '@material-tailwind/react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { StyledEngineProvider } from '@mui/material';
 
 const MaterialProvider = ({ children }: { children: React.ReactNode }) => {
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'Noto Sans KR, sans-serif',
+      fontSize: 14,
+    },
+    // 추가적인 테마 설정...
+  });
+
   return (
     <>
       <StyledEngineProvider injectFirst>
-        <ThemeProvider>
+        <ThemeProvider theme={theme}>
           <CssBaseline />
           {children as any}
         </ThemeProvider>
