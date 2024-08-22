865fc98ea63832bd97575e7dae7a92519cca97aa
Da-Youn <dayoun202@gmail.com>
2024-08-22 19:26:06 +0900

    Feat : 읍면동 주소 검색 시 사용할 오픈 API 경로 재설정 #10

--- app/api/_routes/axiosInstance.ts
+++ app/api/_routes/axiosInstance.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const kakaoInstance = axios.create({
   baseURL: 'http://localhost:3000/api/kakao',
 });
+
+export const mapInstance = axios.create({
+  baseURL: 'http://localhost:3000/api/vworld', // 기본 URL에서 쿼리 파라미터 제외
+});
