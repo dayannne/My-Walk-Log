9de9bf1f325e92ad052cbd998d40b850a14badc5
Da-Youn <dayoun202@gmail.com>
2024-09-02 15:26:07 +0900

    Design : main layout, Navigation 반응형 스타일 수정

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -20,17 +20,30 @@ const MainLayout = ({ children }: MainLayoutProps) => {
     <MapProvider>
       <div className='flex h-full w-full flex-col-reverse bg-white shadow-2xl lg:flex-row'>
         <Navigation />
-        <main className='flex basis-full flex-col overflow-y-hidden lg:flex-row lg:overflow-auto'>
+        <main className='flex basis-full flex-col overflow-y-hidden lg:flex-row'>
           <div
-            className={`sm-md:overflow-y-hidden relative z-20 flex w-full shrink-0 flex-col bg-white lg:flex lg:w-96 lg:min-w-96 lg:basis-auto ${(pathname.includes('profile') || pathname.includes('detail') || pathname.includes('feed')) && 'basis-full'} `}
+            className={`sm-md:overflow-y-hidden relative z-20 flex w-full shrink-0 flex-col bg-white lg:flex lg:w-96 lg:min-w-96 lg:basis-auto ${
+              (pathname.includes('profile') ||
+                pathname.includes('detail') ||
+                pathname.includes('feed')) &&
+              'basis-full'
+            }`}
           >
             {children}
           </div>
           <div
-            className={`relative z-0 flex basis-full overflow-y-hidden lg:flex lg:overflow-auto ${!pathname.includes('place') ? 'hidden' : pathname.includes('detail') ? 'hidden' : ''} `}
+            className={`relative z-0 flex basis-full overflow-y-auto lg:flex lg:overflow-hidden ${
+              !pathname.includes('place')
+                ? 'hidden'
+                : pathname.includes('detail')
+                  ? 'hidden'
+                  : ''
+            }`}
           >
             <div
-              className={`basis-full ${pathname.includes('detail') && 'hidden lg:block'}`}
+              className={`basis-full ${
+                pathname.includes('detail') && 'hidden lg:block'
+              }`}
             >
               <SearchCategory />
               <SearchAgainButton />
