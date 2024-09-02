360f89115edcd194e23fe2664b7e538865ba866a
Da-Youn <dayoun202@gmail.com>
2024-09-02 16:35:16 +0900

    Refactor : 피드 페이지 서버/클라이언트 컴포넌트 분리 #13

--- app/_component/common/Header.tsx
+++ app/_component/common/Header.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import BackButton from './Button/BackButton';
 
 export interface HeaderProps {
   title: string;
@@ -9,24 +9,10 @@ export interface HeaderProps {
 }
 
 const Header = ({ title, children, enableBackButton }: HeaderProps) => {
-  const router = useRouter();
   return (
     <header className='text-olive-green sticky top-0 flex items-center justify-between bg-white p-4 text-base shadow-sm'>
       <div className='flex items-center gap-2'>
-        {enableBackButton && (
-          <button
-            type='button'
-            className='mr-1 flex items-center justify-center border-b border-solid border-gray-200'
-            onClick={() => router.back()}
-          >
-            <Image
-              src='/icons/icon-arrow-left(green).svg'
-              alt='뒤로 가기'
-              width={20}
-              height={20}
-            />
-          </button>
-        )}
+        {enableBackButton && <BackButton />}
         <Link
           className='flex items-center justify-center border-b border-solid border-gray-200 lg:hidden'
           href='/'
