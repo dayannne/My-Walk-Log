625ef97f9391355461a945b30f50023d8c8d29dc
Da-Youn <dayoun202@gmail.com>
2024-09-03 15:12:17 +0900

    Refactor : 장소 상세 불러오기 prefetching 적용 #13

--- app/(main)/(search)/place/search/[keyword]/detail/[placeId]/layout.tsx
+++ app/(main)/(search)/place/search/[keyword]/detail/[placeId]/layout.tsx
@@ -1,35 +1,46 @@
-'use client';
-
-import PlaceDetail from '@/app/_component/place/PlaceDetail';
-import useSmallScreenCheck from '@/app/_hooks/useSmallScreenCheck';
-import { motion } from 'framer-motion';
-import { useState, useEffect } from 'react';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 
 export interface PlaceDetailLayoutProps {
   children: React.ReactNode;
+  params: { placeId: string };
 }
 
-const PlaceDetailLayout = ({ children }: PlaceDetailLayoutProps) => {
-  const { screenSize } = useSmallScreenCheck();
-  const [initialX, setInitialX] = useState<number>(
-    screenSize <= 1023 ? 120 : -120,
-  );
+const PlaceDetailLayout = async ({
+  children,
+  params,
+}: PlaceDetailLayoutProps) => {
+  const { placeId } = params;
+  const queryClient = new QueryClient();
 
-  useEffect(() => {
-    setInitialX(screenSize <= 1023 ? 240 : -120);
-  }, [screenSize]);
-
+  await queryClient.prefetchQuery({
+    queryKey: ['place', placeId],
+    queryFn: async () => {
+      const response = await fetch(`/api/place/${placeId}`);
+      return response.json();
+    },
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ['reviews', placeId],
+    queryFn: async () => {
+      const response = await fetch(`/api/place/${placeId}/review`);
+      return response.json();
+    },
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ['diaries', placeId],
+    queryFn: async () => {
+      const response = await fetch(`/api/place/${placeId}/diary`);
+      return response.json();
+    },
+  });
   return (
-    <motion.div
-      className='sm-md:z-10 absolute top-0 h-full w-full overflow-y-scroll lg:-right-96 lg:-z-10 lg:w-96 lg:overflow-y-hidden lg:py-3 lg:pl-4'
-      initial={{ x: initialX }}
-      animate={{ x: 0 }}
-      transition={{ ease: 'easeOut', duration: 0.5 }}
-    >
-      <div className='h-full w-full rounded-xl border-l border-solid border-gray-200 lg:overflow-hidden lg:shadow-2xl'>
-        {children}
-      </div>
-    </motion.div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {children}
+    </HydrationBoundary>
   );
 };
 
--- app/(main)/(search)/place/search/[keyword]/detail/[placeId]/page.tsx
+++ app/(main)/(search)/place/search/[keyword]/detail/[placeId]/page.tsx
@@ -1,12 +1,35 @@
 'use client';
 
 import PlaceDetail from '@/app/_component/place/PlaceDetail';
+import useSmallScreenCheck from '@/app/_hooks/useSmallScreenCheck';
+import { motion } from 'framer-motion';
+
+import { useEffect, useState } from 'react';
 
 const PlaceDetailPage = ({ params }: { params: { placeId: string } }) => {
   const { placeId } = params;
+  const { screenSize } = useSmallScreenCheck();
+  const [initialX, setInitialX] = useState<number>(
+    screenSize <= 1023 ? 120 : -120,
+  );
+
+  useEffect(() => {
+    setInitialX(screenSize <= 1023 ? 240 : -120);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [screenSize]);
+
   return (
     <>
-      <PlaceDetail placeId={placeId} />
+      <motion.div
+        className='sm-md:z-10 absolute top-0 h-full w-full overflow-y-scroll lg:-right-96 lg:-z-10 lg:w-96 lg:overflow-y-hidden lg:py-3 lg:pl-4'
+        initial={{ x: initialX }}
+        animate={{ x: 0 }}
+        transition={{ ease: 'easeOut', duration: 0.5 }}
+      >
+        <div className='h-full w-full rounded-xl border-l border-solid border-gray-200 lg:overflow-hidden lg:shadow-2xl'>
+          <PlaceDetail placeId={placeId} />
+        </div>
+      </motion.div>
     </>
   );
 };
