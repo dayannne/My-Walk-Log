d672b2a2064ee1e4bfc9d140394d79d528834ca6
Da-Youn <dayoun202@gmail.com>
2024-09-20 15:06:34 +0900

    Chore : API instance에 env 적용(배포/로컬)

--- app/api/_routes/axiosInstance.ts
+++ app/api/_routes/axiosInstance.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const kakaoInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/kakao',
+  baseURL: `${process.env.NEXT_PUBLIC_DOMAIN}/api/kakao`,
 });
 
 export const mapInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/vworld', // 기본 URL에서 쿼리 파라미터 제외
+  baseURL: `${process.env.NEXT_PUBLIC_DOMAIN}/api/vworld`, // 기본 URL에서 쿼리 파라미터 제외
 });
