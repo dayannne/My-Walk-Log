d8b7d89e7581ced85cb4325f6b5e295f0c4969da
Da-Youn <dayoun202@gmail.com>
2024-09-30 14:02:14 +0900

    Fix : API JSON 데이터 변경 적용, 리액트 쿼리 invalidateQueries 추가 적용  #16

--- app/store/server/review.ts
+++ app/store/server/review.ts
@@ -1,7 +1,11 @@
+'use client';
+
 import { IReviewReq } from '@/app/shared/types/review';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { usePlaceDetailStore } from '../client/place';
+import { usePathname } from 'next/navigation';
+import { useModalStore } from '../client/modal';
 
 export const useCreateReview = () => {
   const queryClient = useQueryClient();
@@ -56,6 +60,8 @@ export const useReviewLike = () => {
 
 export const useDeleteReview = () => {
   const queryClient = useQueryClient();
+  const pathname = usePathname();
+  const { openInfo } = useModalStore();
 
   return useMutation({
     mutationFn: async ({
@@ -68,9 +74,13 @@ export const useDeleteReview = () => {
       return await axios.delete(`/api/review/${reviewId}/${userId}/delete`);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['review'] });
-      queryClient.invalidateQueries({ queryKey: ['myProfile'] });
       alert('리뷰가 삭제되었습니다.');
+      // 장소 상세 모달에서 삭제
+      if (openInfo) queryClient.invalidateQueries({ queryKey: ['place'] });
+      // 마이 프로필 페이지에서 삭제
+      if (pathname.includes('profile')) {
+        queryClient.invalidateQueries({ queryKey: ['myProfile'] });
+      }
     },
     onError: (error) => {
       console.log(error);
