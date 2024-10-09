1c95b60403e518051892af98ac4f8535cd848b93
Da-Youn <dayoun202@gmail.com>
2024-10-09 17:20:14 +0900

    Fix : 지도 zoom레벨 변경 시 지도 중복겹침 문제 수정  #16 - kakaoMapRef 설정 - markerRef 설정으로 기존 마커 제거

--- app/_component/diary/SearchWalkedPlace.tsx
+++ app/_component/diary/SearchWalkedPlace.tsx
@@ -2,13 +2,17 @@ import React, { useRef, useState, useEffect } from 'react';
 import { FILTER_CATEGORIES } from '@/app/shared/constant';
 import { filterPlacesByKeyword } from '@/app/shared/function/filter';
 import Image from 'next/image';
+import { IAddressInfo } from '@/app/shared/types/map';
+
 interface Props {
-  selectedPlace: any;
-  setSelectedPlace: (place: any) => void;
+  selectedPlace: IAddressInfo | null;
+  setSelectedPlace: (place: IAddressInfo | null) => void;
 }
 
 const SearchWalkedPlace = ({ selectedPlace, setSelectedPlace }: Props) => {
   const mapRef = useRef<HTMLDivElement>(null);
+  const kakaoMapRef = useRef<kakao.maps.Map | null>(null);
+  const markerRef = useRef<kakao.maps.Marker | null>(null);
   const [places, setPlaces] = useState<any[]>([]);
   const [placeName, setPlaceName] = useState('');
 
@@ -28,11 +32,12 @@ const SearchWalkedPlace = ({ selectedPlace, setSelectedPlace }: Props) => {
       setPlaces([]);
     }
   };
-
   const handleSelectPlace = (place: any) => {
     setPlaceName(place.place_name);
     setSelectedPlace(place);
-    setPlaces([]);
+    setTimeout(() => {
+      setPlaces([]);
+    }, 100);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -42,46 +47,60 @@ const SearchWalkedPlace = ({ selectedPlace, setSelectedPlace }: Props) => {
   };
 
   useEffect(() => {
-    if (selectedPlace && mapRef.current) {
-      const position = new kakao.maps.LatLng(selectedPlace.y, selectedPlace.x);
+    if (!kakaoMapRef.current && mapRef.current) {
       const options = {
-        center: position,
+        center: new kakao.maps.LatLng(37.5665, 126.978),
         level: 5,
       };
-      const kakaoMap = new kakao.maps.Map(mapRef.current, options);
+      kakaoMapRef.current = new kakao.maps.Map(mapRef.current, options);
+    }
+  }, []);
 
-      const imageSrc = '/icons/icon-marker.svg',
-        imageSize = new kakao.maps.Size(56, 56),
-        imageOption = { offset: new kakao.maps.Point(27, 54) };
+  useEffect(() => {
+    if (places.length === 0 && kakaoMapRef.current) {
+      const position = new kakao.maps.LatLng(
+        parseFloat(selectedPlace?.y as string),
+        parseFloat(selectedPlace?.x as string),
+      );
+      kakaoMapRef.current.setCenter(position);
+
+      // 기존 마커를 제거
+      if (markerRef.current) {
+        markerRef.current.setMap(null);
+      }
+
+      const imageSrc = '/icons/icon-marker.svg';
+      const imageSize = new kakao.maps.Size(56, 56);
+      const imageOption = { offset: new kakao.maps.Point(27, 54) };
       const markerImage = new kakao.maps.MarkerImage(
         imageSrc,
         imageSize,
         imageOption,
       );
 
+      // 새 마커 생성
       const marker = new kakao.maps.Marker({
-        map: kakaoMap,
         position: position,
         image: markerImage,
       });
 
-      marker.setMap(kakaoMap);
-      kakaoMap.panTo(position);
+      marker.setMap(kakaoMapRef.current);
+      markerRef.current = marker; // 새 마커를 ref에 저장
     }
-  }, [selectedPlace]);
+  }, [selectedPlace, places]);
 
   return (
     <div className='flex flex-col gap-3'>
       <div className='relative flex max-h-56 w-full flex-col rounded-lg border border-solid border-gray-500 text-sm shadow-sm'>
         <Image
-          className='absolute left-2 top-1/2 w-5 -translate-y-1/2'
+          className='absolute left-2 top-5 w-5 -translate-y-1/2'
           src='/icons/icon-search.svg'
           alt=''
           width={20}
           height={20}
         />
         <input
-          className={`w-full rounded-lg px-2 py-3 pl-8 text-xs focus:outline-none ${places.length > 0 && 'border-b border-solid border-gray-400'}`}
+          className={`w-full rounded-lg px-2 py-3 pl-8 text-xs focus:outline-none ${places.length > 0 && 'rounded-bl-none rounded-br-none border-b border-solid border-gray-300'}`}
           type='text'
           value={placeName}
           onChange={handleSearch}
@@ -93,13 +112,11 @@ const SearchWalkedPlace = ({ selectedPlace, setSelectedPlace }: Props) => {
             {places.map((place) => (
               <li key={place.id}>
                 <button
-                  className='hover:bg-hover group flex w-full cursor-pointer flex-col px-2 py-3'
-                  onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-                    e.preventDefault();
-                    handleSelectPlace(place);
-                  }}
+                  type='button'
+                  className='hover:bg-hover group flex w-full cursor-pointer flex-col px-2 py-2'
+                  onClick={() => handleSelectPlace(place)}
                 >
-                  <span className='place-name'>{place.place_name}</span>
+                  <span className='place-name text-xs'>{place.place_name}</span>
                   <span className='text-xs text-gray-500'>
                     {place.address_name}
                   </span>
